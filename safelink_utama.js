c3adb3fbac78c18fa0b53a4f88661600722771f4
redzaharahap <30423941+redzaharahap@users.noreply.github.com>
2018-06-09 13:23:17 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="donate-and-credit.html"}
 if(x=="2"){xxx="healthy-and-entertainment.html"}
 if(x=="3"){xxx="business-and-law.html"}
 if(x=="4"){xxx="claim-life-insurance.html"}
-if(x=="5"){xxx="car-and-automotive.html"}a_to_ve[i].href="http://www.safelinkos.ga/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="car-and-automotive.html"}a_to_ve[i].href="http://www.safelinkos.ga/2018/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
