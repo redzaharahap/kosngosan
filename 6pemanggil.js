1988950e1de11133fb9bde5aeae314b62dfaaef5
redzaharahap <30423941+redzaharahap@users.noreply.github.com>
2018-09-29 13:26:32 +0700

    Update 6pemanggil.js

--- 6pemanggil.js
+++ 6pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="claim-life-insurance.html"}
 if(x=="2"){xxx="donate-and-credit.html"}
 if(x=="3"){xxx="healthy-and-entertainment.html"}
 if(x=="4"){xxx="car-and-automotive.html"}
-if(x=="5"){xxx="business-and-law.html"}a_to_ve[i].href="http://www.safelinkos.ga/2018/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="business-and-law.html"}a_to_ve[i].href="http://www.safelinkos.com/2018/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
